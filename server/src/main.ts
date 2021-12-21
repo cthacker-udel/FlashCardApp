cf18f64bc48f6140c560d3bb93418d6b4d5523e8
Cameron Thacker <cthacker@udel.edu>
2021-12-21 18:13:39 -0500

    First commit

---
+++ server/src/main.ts
@@ -0,0 +1,20 @@
+import { MongoConfig } from './../config/mongo/mongo.config';
+import { NestFactory } from '@nestjs/core';
+import { ConnectionOptions, createConnection } from 'typeorm';
+import { AppModule } from './app.module';
+const cors = require('cors');
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors();
+    app.use(cors());
+
+    const connection = await createConnection(
+        {...MongoConfig as ConnectionOptions}
+    )
+
+
+    await app.listen(3000);
+}
+bootstrap();
---
+++ server/src/note/note.controller.ts
@@ -0,0 +1,20 @@
+import { NoteService } from './note.service';
+import { Controller, Get } from "@nestjs/common";
+
+
+
+@Controller('note')
+export class NoteController {
+
+    constructor( private readonly noteService: NoteService){}
+
+
+    @Get('all')
+    async getAllCards() {
+
+
+
+    }
+
+
+}
