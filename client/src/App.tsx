71900a03fcbbf8173a6975608c8d63152de74f5d
Cameron Thacker <cthacker@udel.edu>
2022-01-03 23:57:45 -0500

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import { MainPage } from './components/main.components/MainPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useReducer } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { InitialState } from "./assets/InitialState";
+import { DispatchContext } from "./service/dispatch.create.context/DispatchContext";
+import { DispatchFunction } from "./service/dispatch.function/DispatchFunction";
+import { StateContext } from "./service/state.create.context/StateContext";
+import { MainPage } from "./components/main.components/MainPage";
 
-function App() {
-  return (
-    <div className="App">
-      <MainPage />
-    </div>
-  );
-}
+export const App = () => {
+
+    const [state, dispatch] = useReducer(DispatchFunction, InitialState);
+
+    const dispatchValue = { dispatch };
+    const stateValue = { state };
+
+    return(
+        <>
+            <BrowserRouter>
+                <DispatchContext.Provider value={dispatchValue}>
+                    <StateContext.Provider value={stateValue}>
+                        <Routes>
+
+                            <Route path="/mainpage" element={<MainPage />}>
+
+                                <Route path="deletecard" element={<></>} />
+                                <Route path="card-table" element={<></>} />
+                                <Route path="addcard" element={<></>} />
+
+                            </Route>
+
+                            <Route path="/" element={<Navigate to="/mainpage" />} />
+                        </Routes>
+                    </StateContext.Provider>    
+                </DispatchContext.Provider>
+            </BrowserRouter>
+        </>
+    );
+};
 
 export default App;
--- client/src/components/main.components/MainPage.tsx
+++ client/src/components/main.components/MainPage.tsx
@@ -1,38 +1,10 @@
-import React, { useReducer } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { InitialState } from "../../assets/InitialState";
-import { DispatchContext } from "../../service/dispatch.create.context/DispatchContext";
-import { DispatchFunction } from "../../service/dispatch.function/DispatchFunction";
-import { StateContext } from "../../service/state.create.context/StateContext";
+import React from "react";
 
 export const MainPage = () => {
 
-    const [state, dispatch] = useReducer(DispatchFunction, InitialState);
-
-    const dispatchValue = { dispatch };
-    const stateValue = { state };
-
     return(
         <>
-            <BrowserRouter>
-                <DispatchContext.Provider value={dispatchValue}>
-                    <StateContext.Provider value={stateValue}>
-                        <Routes>
-
-                            <Route path="/mainpage" element={<MainPage />}>
-
-                                <Route path="deletecard" element={<></>} />
-                                <Route path="card-table" element={<></>} />
-                                <Route path="addcard" element={<></>} />
-
-                            </Route>
-
-                            <Route path="/" element={<Navigate to="/mainpage" />} />
-                        </Routes>
-                    </StateContext.Provider>    
-                </DispatchContext.Provider>
-            </BrowserRouter>
+            MainPage goes here
         </>
     );
 };
